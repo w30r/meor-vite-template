0532d6585ce3607dbeb18ab0fde71a738d24010a
SyamilEftech <mr.meorsyamil@gmail.com>
2025-06-21 20:23:11 +0800

    My FE template is done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1>VITE + TAILWINDCSS</h1>
+      <h1>SHADCN + REACT</h1>
+      <p className="text-md">Meor's Template for frontend development.</p>
+    </div>
+  );
+}
+
+export default App;
